607e2a5dcad2312e4553f4aa792af22660758a12
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-02-06 22:54:04 -0500

    initial harmonics knowledge base

---
+++ src/MusicAPI/harmonicsKB/Triad.java
@@ -0,0 +1,14 @@
+package MusicAPI.harmonicsKB;
+
+/**
+ * Created by ben on 5/10/2015.
+ */
+public enum Triad {
+    Major,
+    Minor,
+    Augmented,
+    Diminished,
+    Dominant,
+    Suspended2nd,
+    Suspended4th;
+}
