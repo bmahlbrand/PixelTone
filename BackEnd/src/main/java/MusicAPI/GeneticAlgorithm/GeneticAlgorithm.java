b1be24a04fddbd3b69e3e1cba11f1ad5bd0b2724
austin-rauschuber <arauschu@purdue.edu>
2016-02-23 13:11:58 -0500

    Add Generic Genetic Algorithm and Update Package Structure

---
+++ BackEnd/src/main/java/MusicAPI/GeneticAlgorithm/GeneticAlgorithm.java
@@ -0,0 +1,54 @@
+package MusicAPI.GeneticAlgorithm;
+
+import java.util.*;
+import org.jenetics.*;
+import org.jenetics.util.*;
+import org.jenetics.engine.Engine;
+import static org.jenetics.engine.EvolutionResult.toBestPhenotype;
+import static org.jenetics.engine.limit.bySteadyFitness;
+
+public class GeneticAlgorithm{
+
+	 public static void enumgeneticAlgorithm(ArrayList<Enum> myEnum, int length, FitnessFunction fitFunctionInstance){
+
+        geneticAlgorithm(0,myEnum.size()-1,length,fitFunctionInstance);
+
+    }
+
+    public static void geneticAlgorithm(int minVal, int maxVal, int length, FitnessFunction fitFunctionInstance){
+
+    	final Engine<IntegerGene, Integer> engine = Engine
+    		.builder(
+    				fitFunctionInstance :: fitnessFunction,
+    				IntegerChromosome.of(minVal, maxVal, length))
+    		.optimize(Optimize.MAXIMUM)
+    		.maximalPhenotypeAge(11)
+    		.populationSize(500)
+    		.alterers(
+    			new Mutator<>(0.115),
+    			new SinglePointCrossover<>(0.16))
+    		.build();
+
+    	final Phenotype<IntegerGene, Integer> best =
+    		engine.stream()
+    		.limit(bySteadyFitness(15))
+    		.limit(250)
+    		.collect(toBestPhenotype());
+
+    	System.out.println(best); 
+    }
+
+    public static ArrayList<Enum> IntegertoEnum(final Genotype<IntegerGene> gt, ArrayList<Enum> myEnum){
+        final int[] intSet = gt.getChromosome().toSeq().stream().mapToInt(IntegerGene:: getAllele).toArray();
+        ArrayList<Enum> enumList = new ArrayList<Enum>();
+        for (int count = 0; count < intSet.length; count++){
+            enumList.add(myEnum.get(intSet[count]));
+        }
+
+        return enumList;
+    }
+
+    public static final int[] GenotypeToIntArray(final Genotype<IntegerGene> gt){
+        return gt.getChromosome().toSeq().stream().mapToInt(IntegerGene:: getAllele).toArray();
+    }
+}
--- BackEnd/src/main/java/MusicAPI/main.java
+++ BackEnd/src/main/java/MusicAPI/main.java
@@ -1,11 +1,14 @@
-package hello;
+package MusicAPI;
 
 import static spark.Spark.*;
 
 public class main {
 	public static void main(String[] args) {
+
         get("/generateSong", (request, response) -> {
-        	//insert song generation method here (or whichever method returns the song link)
-        }
-    }
+            return "Placeholder";
+        });
+
+
+    }        
 }
