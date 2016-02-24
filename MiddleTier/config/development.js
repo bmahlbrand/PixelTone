ca940ee0c7f0f98bc8aa2eb35239c13efc867400
Jacob Richwine <jrichwin@purdue.edu>
2016-02-23 21:01:47 -0500

    Sprint 1 Demo Ready

--- MiddleTier/config/development.js
+++ MiddleTier/config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MSEmotionPrimeKey: "DO_NOT_COMMIT_THIS_KEY",
-    MSEmotionSecKey: "DO_NOT_COMMIT_THIS_KEY",
+    MSEmotionPrimeKey: "",
+    MSEmotionSecKey: "DO_NOT_COMMIT_THIS_KEY_OR_ABOVE",
     mongodb: "mongodb://127.0.0.1:27017/PixelTone"
 }
