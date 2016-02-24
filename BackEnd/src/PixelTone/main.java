b8def46db38c646944ac3c6e9aa7fba3b3b33403
Jacob Richwine <jrichwin@purdue.edu>
2016-02-23 21:00:38 -0500

    Cleanup For Sprint 1 Demo

--- BackEnd/src/PixelTone/main.java
+++ BackEnd/src/PixelTone/main.java
@@ -22,8 +22,29 @@ public class main {
         try{
             Gson gson = new Gson();
             GenerationParams gp = gson.fromJson(params, GenerationParams.class);
-            System.out.println(params);
+            //System.out.println(params);
 
+            System.out.println("Parameters Received");
+            System.out.println("------------------------------------");
+            System.out.println("Facial Information");
+            System.out.println("Number of Faces:" + gp.numberOfFaces);
+            int i = 0;
+            for(Face f : gp.faces)
+            {
+                System.out.println("Face #" + i++ + ":\n");
+                for( Emotion e : f.emotions)
+                {
+                    System.out.println("Facial Emotion:" + e.emotion);
+                    System.out.println("Emotion Value:" + e.value);
+                }
+                System.out.println("------------------------------------");
+            }
+
+            System.out.println("\nImage Color Information\n");
+            System.out.println("Dominant Color:" + gp.domColor);
+            System.out.println("#1 Palette Color:" + gp.pal1);
+            System.out.println("#2 Palette Color:" + gp.pal2);
+            System.out.println("#3 Palette Color:" + gp.pal3);
             //Ship to MoodToMusic to change the moods/emotions to music
             //Store song in database?
             //????? TBD
--- MiddleTier/config/development.js
+++ MiddleTier/config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MSEmotionPrimeKey: "",
-    MSEmotionSecKey: "",
-    mongodb: "mongodb://127.0.0.1/test"
+    MSEmotionPrimeKey: "DO_NOT_COMMIT_THIS_KEY",
+    MSEmotionSecKey: "DO_NOT_COMMIT_THIS_KEY",
+    mongodb: "mongodb://127.0.0.1:27017/PixelTone"
 }
