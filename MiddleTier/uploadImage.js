9b3b4c12fba76b441a11523b70266cd46360f77c
Paul Ryan <paulpjryan@gmail.com>
2016-03-25 10:40:36 -0400

    make image upload into function

--- MiddleTier/uploadImage.js
+++ MiddleTier/uploadImage.js
@@ -4,26 +4,29 @@ var keygen = require('random-key');
 
 AWS.config.region = 'us-east-1';
 
-var s3bucket = new AWS.S3({params: {Bucket: 'pixeltone'}});
-var body = fs.createReadStream('./testImg.jpg');
-var myKey = keygen.generateBase30(20) + '.jpg';
+var uploadImage = function(file) {
+	var s3bucket = new AWS.S3({params: {Bucket: 'pixeltone'}});
+	var body = fs.createReadStream(file);
+	var myKey = keygen.generateBase30(20) + '.jpg';
+	
+	s3bucket.createBucket(function() {
+		var params = {	
+			Key: myKey,
+			Body: body,
+			ContentType: 'image/jpeg',
+			ACL: 'public-read'
+		};
+		s3bucket.upload(params).on('httpUploadProgress', function(evt) {
+				//console.log(evt);
+				console.log(Math.round((evt.loaded / evt.total) * 100) + '% uploaded');
+				if(evt.loaded == evt.total) {
+					console.log('Uploaded file to https://s3.amazonaws.com/pixeltone/' + myKey);
+				}
+			}).send(); 
+	});
+};
 
-s3bucket.createBucket(function() {
-	var params = {	
-		Key: myKey,
-		Body: body,
-		ContentType: 'image/jpeg',
-		ACL: 'public-read'
-	};
-	s3bucket.upload(params).on('httpUploadProgress', function(evt) {
-			//console.log(evt);
-			console.log(Math.round((evt.loaded / evt.total) * 100) + '% uploaded');
-			if(evt.loaded == evt.total) {
-				console.log('Uploaded file to https://s3.amazonaws.com/pixeltone/' + myKey);
-			}
-		}).send(); 
-});
-
+uploadImage('./testImg2.jpg');
 //s3bucket.getObject({Key: myKey}, function(err, data) {
 //	if (err) 
 //		console.log(err, err.stack); // an error occurred
