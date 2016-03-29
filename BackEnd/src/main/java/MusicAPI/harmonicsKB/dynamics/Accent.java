97e53b0ac3baa61b569bf50e567bd7ad19cffce3
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-03-28 21:44:50 -0400

    accents update

--- BackEnd/src/main/java/MusicAPI/harmonicsKB/dynamics/Accent.java
+++ BackEnd/src/main/java/MusicAPI/harmonicsKB/dynamics/Accent.java
@@ -1,19 +1,47 @@
 package MusicAPI.harmonicsKB.dynamics;
 
+import MusicAPI.harmonicsKB.rhythm.BeatDuration;
+import MusicAPI.structure.Beat;
+
 /**
  * Created by ben on 3/5/2016.
  */
 public enum Accent {
-    Staccato,
-    Staccatissimo,
-    Marcato,
-    Accent,
-    Tenuto,
+    Staccato, //last part of note should be silenced
+    Staccatissimo, //shorter staccato
+    Marcato, //loud as an accent, short as staccato
+    Accent, //emphasized beginning and taper off quickly
+    Tenuto, //multiple meanings, but lets assume "doo" in jazz articulation ie slight separation
     Unaccented;
 
-    int getDuration() {
+
+
+    public int getDuration(BeatDuration duration) {
         // TODO add handling for padding total note duration and the "on" duration for accenting
-        return 0;
+        int ret = 0;
+
+        switch(this) {
+            case Staccato:
+                ret = duration.getNumberOfSixtyFourthNotes() * 5;
+                break;
+            case Staccatissimo:
+                ret = duration.getNumberOfSixtyFourthNotes() * 2;
+                break;
+            case Marcato:  //TODO handle loudness
+                ret = duration.getNumberOfSixtyFourthNotes() * 5;
+                break;
+            case Accent:
+
+                break;
+            case Tenuto:
+
+                break;
+            case Unaccented:
+                ret = duration.getNumberOfSixtyFourthNotes() * 6;
+                break;
+        }
+
+        return ret;
     }
 
 }
