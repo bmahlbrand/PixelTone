79cb9a1c4ef569907cfef18fd7b8edb00dcd073e
austin-rauschuber <arauschu@purdue.edu>
2016-04-27 16:28:53 -0400

    Assign Accompaniment Styles To Tempo

--- BackEnd/src/main/java/MusicAPI/virtuouso/models/genetic/GeneticSimpleComposition.java
+++ BackEnd/src/main/java/MusicAPI/virtuouso/models/genetic/GeneticSimpleComposition.java
@@ -1,5 +1,7 @@
 package MusicAPI.virtuouso.models.genetic;
 
+import java.util.Random;
+
 import MusicAPI.structure.*;
 import MusicAPI.harmonicsKB.scale.*;
 import MusicAPI.harmonicsKB.triads.Triad;
@@ -16,6 +18,10 @@ public class GeneticSimpleComposition{
 	Mode mode;
 	int tempo;
 	Composition generatedSong;
+
+	final int SAD_TEMPO = 70;
+	final int NEUTRAL_TEMPO = 100;
+	final int HAPPY_TEMPO = 120;
 
 	public GeneticSimpleComposition(){
 		key = new Note("C");
@@ -46,6 +52,8 @@ public class GeneticSimpleComposition{
 		for (int i=0; i<2; i++){
 			currentChordProgression = generateChordProgression(i);
 			currentMelody = generateMelody(currentChordProgression);
+
+			currentChordProgression = setChordProgressionRhythm(currentChordProgression);
 
 			if (i==0){
 				aSectionMelody = currentMelody;
@@ -122,5 +130,30 @@ public class GeneticSimpleComposition{
 			newSection.addMeasure(AccompanimentRhythm.eighthNoteBass(currentMeasure));
 		}
 		return newSection;
+	}
+
+	private Section setChordProgressionRhythm(Section chordProgression){
+		Random rng = new Random();
+		int randomBoolean = rng.nextBoolean();
+		if (tempo <= SAD_TEMPO){
+			if (randomBoolean)
+				return makeAccompanimentArpeggiated(chordProgression);
+			return makeAccompanimentAlbertiBass(chordProgression);
+		}
+
+		if (tempo <= NEUTRAL_TEMPO){
+			if (randomBoolean)
+				return makeAccompanimentQuarterNotes(chordProgression);
+			return chordProgression;
+
+		}
+		if (tempo <= HAPPY_TEMPO){
+			if (randomBoolean)
+				return makeAccompanimentQuarterNotes(chordProgression);
+			return makeAccompanimentAlbertiBass(chordProgression);
+		}
+		if (randomBoolean)
+			return makeAccompanimentEighthNotes(chordProgression);
+		return makeAccompanimentQuarterNotes(chordProgression);
 	}
 }
