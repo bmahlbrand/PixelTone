0c42b3cebd8ff292e8e6fd432f920a902553b86c
Paul Ryan <paulpjryan@gmail.com>
2016-04-28 10:20:05 -0400

    Improve flow for midi upload

--- MiddleTier/uploadSong.js
+++ MiddleTier/uploadSong.js
@@ -8,8 +8,6 @@ var exports = module.exports = {};
 
 //configure aws
 AWS.config.loadFromPath('./config/awsConfig.json');
-//connect to mongodb
-//mongoose.connect("mongodb://127.0.0.1:27017/PixelTone");
 
 exports.uploadSong = function(file, user, songKey) {
 	var s3bucket = new AWS.S3({params: {Bucket: 'pixeltone-midi'}});
@@ -25,31 +23,9 @@ exports.uploadSong = function(file, user, songKey) {
 		};
 		console.log('Uploading midi to S3...');
 		s3bucket.upload(params).on('httpUploadProgress', function(evt) {
-				//console.log(evt);
 				//console.log(Math.round((evt.loaded / evt.total) * 100) + '% uploaded');
 				//100% uploaded
 				if(evt.loaded == evt.total) {
-
-					//console.log("Creating new image in database");
-
-					var newSong = new Song();
-					newSong.local.key = songKey;
-					newSong.local.url = 'https://s3.amazonaws.com/pixeltone-midi/' + filename;
-					newSong.local.ntsurl = 'https://s3.amazonaws.com/pixeltone-midi/' + filename + ".NTS";
-
-					//console.log(newImage);
-					console.log("Saving to db: " + newSong);
-					newSong.save(function(err) {
-						if (err) {
-							console.log("Error occurred");
-                            throw err;
-                        }
-                        else {
-                            console.log("Song Saved");
-                            //return;
-                        }
-					});
-
 					console.log('Uploaded file to https://s3.amazonaws.com/pixeltone-midi/' + filename);
 				}
 			}).send();
@@ -64,10 +40,26 @@ exports.uploadSong = function(file, user, songKey) {
 		};
 		console.log('Uploading midi.NTS to S3...');
 		s3bucket.upload(params).on('httpUploadProgress', function(evt) {
-				//console.log(evt);
 				//console.log(Math.round((evt.loaded / evt.total) * 100) + '% uploaded');
 				//100% uploaded
 				if(evt.loaded == evt.total) {
+
+					var newSong = new Song();
+					newSong.local.key = songKey;
+					newSong.local.url = 'https://s3.amazonaws.com/pixeltone-midi/' + filename;
+					newSong.local.ntsurl = 'https://s3.amazonaws.com/pixeltone-midi/' + filename + ".NTS";
+
+					console.log("Saving to db: " + newSong);
+					newSong.save(function(err) {
+						if (err) {
+							console.log("Error occurred");
+                            throw err;
+                        }
+                        else {
+                            console.log("Song Saved");
+                            //return;
+                        }
+					});
 					console.log('Uploaded file to https://s3.amazonaws.com/pixeltone-midi/' + filename + ".NTS");
 				}
 			}).send();
@@ -75,11 +67,3 @@ exports.uploadSong = function(file, user, songKey) {
 
 
 };
-
-//uploadImage('./testImg2.jpg');
-//s3bucket.getObject({Key: myKey}, function(err, data) {
-//	if (err)
-//		console.log(err, err.stack); // an error occurred
-//	else
-//		console.log(data);           // successful response
-//});
