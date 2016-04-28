a22f11f6620047fe7a19ed7b3087a4b457db639d
John Masterson <fattyforhire@Johns-MacBook-Pro.local>
2016-04-27 22:18:50 -0400

    Added basic music player controls and progress bar for song

--- website/app/songViewer/songViewerController.js
+++ website/app/songViewer/songViewerController.js
@@ -1,8 +1,9 @@
 angular.module("pixelTone")
 .controller('songViewerController', ['$scope', '$http', 'AuthService', function ($scope, $http, AuthService) {
   $scope.song = new composition();
-  $scope.isPlaying = false;
   // get specific song
+
+
   $scope.displaySongOnCanvas = function () {
     //delete the stupid canvas
     var canvasDiv = document.getElementById("vexflow-canvas-div");
@@ -49,21 +50,47 @@ angular.module("pixelTone")
             player.start();
             player.stop();
           });
+		      var musicbarinner = document.getElementById("music-bar-inner");
+		      var musicbarouter = document.getElementById("music-bar-outer");
+          var timeleft = document.getElementById("time-left");
+          var timeplayed = document.getElementById("time-played");
+          player.setAnimation(function(data, element) {
+
+                  var timeFormatting = function(n) {
+              			var minutes = n / 60 >> 0;
+              			var seconds = String(n - (minutes * 60) >> 0);
+              			if (seconds.length == 1) seconds = "0" + seconds;
+              			return minutes + ":" + seconds;
+              		}
+
+          			var percent = data.now / data.end;
+          			var now = data.now >> 0; // where we are now
+          			var end = data.end >> 0; // end of song
+          			// display the information to the user
+          			musicbarinner.style.width = (percent * 100) + "%";
+                timeplayed.innerHTML = timeFormatting(now);
+                timeleft.innerHTML = "-" + timeFormatting(end - now);
+          		});
         }
       });
     };
 
     $scope.pausePlayButton = function() {
-      if($scope.isPlaying) {
+      if(MIDI.Player.playing) {
         MIDI.Player.pause(true);
         $("#pausePlayIcon").addClass('glyphicon-play').removeClass('glyphicon-pause');
-        $scope.isPlaying = false;
       }else{
         player.resume();
         $("#pausePlayIcon").addClass('glyphicon-pause').removeClass('glyphicon-play');
-        $scope.isPlaying = true;
       }
     }
+
+    $scope.stopButton = function() {
+      MIDI.Player.stop();
+      $("#pausePlayIcon").addClass('glyphicon-play').removeClass('glyphicon-pause');
+    }
+
+
 
     $scope.fetchSong = function (song) {
       $http.get("/songs/notes/"+song+".mid.NTS")
